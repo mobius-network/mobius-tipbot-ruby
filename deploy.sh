8ebb7b4a4dccb06ef74356ad6a4300ad5ff1eb00
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-04-06 20:23:57 +0400

    Deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+	docker build -t gcr.io/mobius-network/hello-app:${REVISION} .
+	gcloud docker -- push gcr.io/mobius-network/hello-app:${REVISION}
+
+	cat manifests/helloweb-deployment.yaml | sed 's/__REVISION__/'"$REVISION"'/g' > manifests/helloweb-deployment-r.yaml
+
+	kubectl apply -f manifests/helloweb-deployment-r.yaml
+	kubectl apply -f manifests/helloweb-service.yaml
+
+	kubectl get pods
+}
+
+main "$@"
---
+++ deploy/setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+	# TODO: replace the project with your project ID
+	local project=mobius-network
+
+	# Pre-req for gcloud install
+	sudo apt-get update
+	sudo apt-get install -y apt-transport-https
+
+	# Copied from the official install instructions on https://cloud.google.com/sdk/downloads#apt-get
+	export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+	echo "deb https://packages.cloud.google.com/apt ${CLOUD_SDK_REPO} main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install -y google-cloud-sdk kubectl
+
+	gcloud --version
+	kubectl version -c
+
+	echo "Authenticating gcloud service account"
+	gcloud auth activate-service-account \
+		--key-file "${GOOGLE_APPLICATION_CREDENTIALS}" \
+		--project "${project}"
+
+	echo "Authenticating to GCR"
+	gcloud docker --authorize-only --project "${project}"
+
+	echo "Configuring kubectl"
+	gcloud container clusters get-credentials hello-cluster \
+		--project "${project}" \
+		--zone us-central1-b
+}
+
+main "$@"
