f1a8b9d4a8790b8142cdfdc3eb2d64a7db5b3ffb
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-04-06 20:41:36 +0400

    Fix setup script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -11,14 +11,14 @@ main() {
 	sudo apt-get install -y apt-transport-https
 
 	# Copied from the official install instructions on https://cloud.google.com/sdk/downloads#apt-get
-	export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release --client -s)"
+	export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 	echo "deb https://packages.cloud.google.com/apt ${CLOUD_SDK_REPO} main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 	sudo apt-get update
 	sudo apt-get install -y google-cloud-sdk kubectl
 
 	gcloud --version
-	kubectl version -c
+	kubectl version --client
 
 	echo "Authenticating gcloud service account"
 	gcloud auth activate-service-account \
