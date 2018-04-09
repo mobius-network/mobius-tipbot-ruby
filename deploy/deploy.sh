585760a0118bc285082861b723953cd25667293d
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-04-09 22:05:27 +0400

    Telegram deployment

--- deploy/deploy.sh
+++
@@ -1,5 +0,0 @@
-docker build -t gcr.io/mobius-network/hello-app:${REVISION} .
-gcloud docker -- push gcr.io/mobius-network/hello-app:${REVISION}
-cat manifests/helloweb-deployment.yaml | sed 's/__REVISION__/'"$REVISION"'/g' > manifests/helloweb-deployment-r.yaml
-kubectl apply -f manifests/helloweb-deployment-r.yaml
-kubectl apply -f manifests/helloweb-service.yaml
