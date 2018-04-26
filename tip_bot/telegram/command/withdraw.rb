492884a739b36b891c1d6757c282333d1301fd3f
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-04-26 17:57:50 +0400

    Handling of some Stellar errors

--- tip_bot/telegram/command/withdraw.rb
+++ tip_bot/telegram/command/withdraw.rb
@@ -8,12 +8,40 @@ class TipBot::Telegram::Command::Withdraw < TipBot::Telegram::Command::Base
   private
 
   def reply
-    return t(:address_missing) if address.nil?
-    return t(:nothing) if user.balance.zero?
+    return say_address_missing if address.nil?
+    return say_nothing_to_withdraw if user.balance.zero?
     user.withdraw(address)
-    t(:done, address: address)
+    say_done
+  rescue Mobius::Client::Error::TrustlineMissing
+    say_trustline_missing
+  rescue Mobius::Client::Error::AccountMissing
+    say_account_missing
   rescue Mobius::Client::Error::UnknownKeyPairType
+    say_invalid_address
+  end
+
+  def say_trustline_missing
+    t(:trustline_missing, address: address, code: Mobius::Client.asset_code, issuer: Mobius::Client.asset_issuer)
+  end
+
+  def say_account_missing
+    t(:account_missing, address: address)
+  end
+
+  def say_invalid_address
     t(:invalid_address, address: address)
+  end
+
+  def say_done
+    t(:done, address: address)
+  end
+
+  def say_address_missing
+    t(:address_missing)
+  end
+
+  def say_nothing_to_withdraw
+    t(:nothing)
   end
 
   def address
