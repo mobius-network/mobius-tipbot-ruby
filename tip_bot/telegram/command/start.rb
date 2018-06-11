b5e3272132ee05b2a2769fd706a9cc50c19e1418
Timur Ramazanov <charlie.vmk@gmail.com>
2018-06-11 13:25:13 +0600

    Fix /help command

--- tip_bot/telegram/command/start.rb
+++ tip_bot/telegram/command/start.rb
@@ -2,7 +2,7 @@
 class TipBot::Telegram::Command::Start < TipBot::Telegram::Command::Base
   def call
     type = direct_message? ? :private : :public
-    text = TipBot.t(type, username: from.username)
+    text = t(type, username: from.username)
     bot.api.send_message(chat_id: chat.id, text: text)
   end
 end
