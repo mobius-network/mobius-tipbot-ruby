2301d4de89308f8b8544e8c14fdc40c5ff736008
Sergey Nebolsin <sergey@nebols.in>
2019-05-02 04:19:02 +0300

    Travis: fix VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ end
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.default_cassette_options = { record: :new_episodes }
   config.hook_into :webmock
 end
 
